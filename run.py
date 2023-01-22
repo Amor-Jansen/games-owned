c2080faa9e6bbfc31708ce3b2c420060f5710ce9
Amor-Jansen <Amor.jansen2@gmail.com>
2023-01-22 14:31:19 +0000

    Update credentials

--- run.py
+++ run.py
@@ -1,1 +1,10 @@
 import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+SHEET = GSPREAD_CLIENT.open('games_owned')
