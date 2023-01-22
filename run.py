25e1d21f9b2ddd64f688f82884ae46defa60e793
Amor-Jansen <Amor.jansen2@gmail.com>
2023-01-22 15:10:26 +0000

    Add welcome message and name conventions

--- run.py
+++ run.py
@@ -13,7 +13,18 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('games_owned')
 
 ps4 = SHEET.worksheet('ps4')
+ps3 = SHEET.worksheet('ps3')
 
-data = ps4.get_all_values()
+# Welcome message
 
-print(data)
+print('Welcome to your games owned list.')
+print('Here you can view all the \n video games you own.')
+print('You can also update your list.')
+print('To make sure you do not buy doubles. \n')
+
+# Name request
+
+print('Please enter your name.')
+print('It must be a valid name at least 8 characters long.')
+print('only A-z, a-z and 0-8 will be accepted')
+print('Blank space will be removed')
