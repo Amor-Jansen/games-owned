603d22a2f1f297ac5ffa91d510199a891651c5bb
Amor-Jansen <Amor.jansen2@gmail.com>
2023-01-22 14:59:32 +0000

    Link APIs and confirm access to google sheets

--- run.py
+++ run.py
@@ -7,4 +7,13 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('games_owned')
+
+ps4 = SHEET.worksheet('ps4')
+
+data = ps4.get_all_values()
+
+print(data)
